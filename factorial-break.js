3271c8c7b85b3eebff79dfa962fffb02d16335f6
Roxanarhenals25 <roxanarhenals65@gmail.com>
2023-05-16 16:03:58 -0500

    bucles for, while y break

---
+++ factorial-break.js
@@ -0,0 +1,8 @@
+// Este archivo debe calcular el factorial de 10 utilizando un bucle while, una bifurcación if y una sentencia break
+
+let i = 0;
+let m = 10;
+while ( i < m){
+    if ( i == m)
+    break;
+}
---
+++ factorial-while.js
@@ -0,0 +1,7 @@
+// Este archivo debe calcular el factorial de 10 utilizando un bucle while
+
+let i = 0;
+let max = 10;
+while (i < max){
+    console.log(i); i++;
+}
