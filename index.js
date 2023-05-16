bfdbfd3c34de23ab4e7e6aaa33c7546414a969bb
Roxanarhenals25 <roxanarhenals65@gmail.com>
2023-05-16 13:19:54 -0500

    segundo ejercicio de js creando listas y objetos

---
+++ index.js
@@ -0,0 +1,14 @@
+const particulars = [ 
+    {
+        name: "Roxana Rhenals",
+        age: 23,
+        isDeveloper: true,
+        date : new Date(2000,0,25),
+        favBook: {
+            titulo: "Los cuatro acuerdos",
+            autor: "Dr Miguel Ruiz",
+            date: new Date(1997)
+        }
+    }
+]
+console.log(favBook.date)
